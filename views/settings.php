006a4f0e868df441acdbbbef5e1b2fb06a9aa1c3
Alessandro Miliucci <lifeisfoo@gmail.com>
2013-05-17 00:29:49 +0200

    fixed typo

--- views/settings.php
+++ views/settings.php
@@ -2,4 +2,4 @@
 
 <?php $this->ConfigurationModule->Render(); ?>
 
-<h1>Pay attenction to super roles! Administrator can't be selected ;-)</h1>
+<h1>Pay attention to super roles! Administrator can't be selected ;-)</h1>
